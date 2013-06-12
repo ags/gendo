3f61ab6860b5d5f983da4b649236cca301da6161
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-06-13 00:11:11 +1000

    Implement Transaction#recent spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Transaction do
   describe "#recent" do
-    it
+    it "returns a requested number of the most recent Transactions" do
+      a = Transaction.make!
+      b = Transaction.make!
+      c = Transaction.make!
+
+      expect(Transaction.recent(2)).to eq([c, b])
+    end
   end
 end
