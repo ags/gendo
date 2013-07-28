f6f2bfd3a76508421204c0da402e11ca0a126d89
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-07-29 00:29:26 +1000

    Alphabetize blueprints.

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,27 +1,5 @@
 require "machinist/active_record"
 
-User.blueprint do
-  email    { "user#{sn}@blargh.com" }
-  password { "password" }
-end
-
-AppAccessToken.blueprint do
-  app
-  token { sn }
-end
-
-Transaction.blueprint do
-  source
-end
-
-Source.blueprint do
-  app
-  controller    { "PostsController" }
-  action        { "new" }
-  method_name   { "GET" }
-  format_type   { "*/*" }
-end
-
 App.blueprint do
   user
   name { "Shinji Ikari" }
@@ -29,6 +7,11 @@ end
 
 App.blueprint(:with_access_token) do
   app_access_tokens { [AppAccessToken.make!] }
+end
+
+AppAccessToken.blueprint do
+  app
+  token { sn }
 end
 
 MailerEvent.blueprint do
@@ -43,4 +26,21 @@ end
 NPlusOneQuery.blueprint do
   transaction
   culprit_table_name { "posts" }
+end
+
+Source.blueprint do
+  app
+  controller    { "PostsController" }
+  action        { "new" }
+  method_name   { "GET" }
+  format_type   { "*/*" }
+end
+
+Transaction.blueprint do
+  source
+end
+
+User.blueprint do
+  email    { "user#{sn}@blargh.com" }
+  password { "password" }
 end
