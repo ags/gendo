4515cdfe9b658ea6db9697591977d869b281270c
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-07-28 19:38:54 +1000

    Basic n+1 query identifier.

---
+++ lib/gendo/identifies_n_plus_one_queries.rb
@@ -0,0 +1,57 @@
+module Gendo
+  class IdentifiesNPlusOneQueries
+    def self.identify(sql_events, options={})
+      new(sql_events, options).identify
+    end
+
+    def initialize(sql_events, options={})
+      @sql_events = sql_events
+      @minimum_query_occurances = options.fetch(
+        :minimum_query_occurances,
+        DEFAULT_MINIMUM_QUERY_OCCURANCES
+      )
+    end
+
+    def identify
+      potential_events_by_table.map do |table_name, sql_events|
+        if sql_events.size >= @minimum_query_occurances
+          PotentialNPlusOneQuery.new(table_name, sql_events)
+        end
+      end.compact
+    end
+
+    private
+
+    def potential_events_by_table
+      potentials = {}
+      @sql_events.each do |event|
+        if matches = POTENTIAL_MATCHER.match(event.sql)
+          table_name = matches[:table_name]
+          potentials[table_name] ||= []
+          potentials[table_name] << event
+        end
+      end
+      potentials
+    end
+
+    DEFAULT_MINIMUM_QUERY_OCCURANCES = 5
+
+    POTENTIAL_MATCHER =
+      /
+      # select from
+      SELECT .* \W*
+
+      # table name
+      FROM \W* (?<table_name>\w*) \W*
+
+      # where a column is equal to a digit or a binding
+      WHERE .*\W*=\W*(\d+) \W*
+
+      # limited to one row
+      LIMIT \W* 1
+      /x
+  end
+end
+
+class PotentialNPlusOneQuery < Struct.new(:table_name, :events)
+end
---
+++ spec/lib/gendo/identifies_n_plus_one_queries.rb
@@ -0,0 +1,87 @@
+require_relative "../../../lib/gendo/identifies_n_plus_one_queries"
+
+describe Gendo::IdentifiesNPlusOneQueries do
+  it "returns the format of queries" do
+    foo_event = double(
+      "n+1 foo event",
+      sql: "SELECT `foos`.* FROM `foos` WHERE `foos`.`id` = 13 LIMIT 1"
+    )
+
+    bar_event = double(
+      "n+1 bar event",
+      sql: "SELECT `bars`.* FROM `bars` WHERE `bars`.`id` = 27 LIMIT 1"
+    )
+
+    baz_event = double(
+      "baz event",
+      sql: "INSERT INTO `bars` (`col`) VALUES (123)"
+    )
+
+    identified = Gendo::IdentifiesNPlusOneQueries.identify(
+      [foo_event, bar_event, baz_event],
+      minimum_query_occurances: 1
+    )
+
+    expect(identified).to eq([
+      PotentialNPlusOneQuery.new("foos", [foo_event]),
+      PotentialNPlusOneQuery.new("bars", [bar_event]),
+    ])
+  end
+
+  it "by default requires there to be 5 or more potential queries on a table" do
+    queries = []
+    n_plus_one_events = 5.times.map do |id|
+      event = double(
+        "n+1 event #{id}",
+        sql: "SELECT `foos`.* FROM `foos` WHERE `foos`.`id` = #{id} LIMIT 1"
+      )
+      queries << event
+      event
+    end
+
+    4.times do
+      queries << double(
+        "n+1 format, lone event for different table",
+        sql: "SELECT `bars`.* FROM `bars` WHERE `bars.`id` = 27 LIMIT 1"
+      )
+    end
+
+    identified = Gendo::IdentifiesNPlusOneQueries.identify(queries)
+
+    expect(identified).to eq([
+      PotentialNPlusOneQuery.new("foos", n_plus_one_events)
+    ])
+  end
+
+  it "correctly identifies parameterized queries" do
+    query = double(
+      "parameterized query",
+      sql: "SELECT `bars`.* FROM `bars` WHERE `bars.`id` = $1 LIMIT 1"
+    )
+
+    identified = Gendo::IdentifiesNPlusOneQueries.identify(
+      [query],
+      minimum_query_occurances: 1
+    )
+
+    expect(identified).to eq([
+      PotentialNPlusOneQuery.new("bars", [query])
+    ])
+  end
+
+  it "ignores whitespace between statements" do
+    query = double(
+      "parameterized query",
+      sql: "SELECT   `bars`.*   FROM   `bars`   WHERE `bars.`id` = 27   LIMIT 1"
+    )
+
+    identified = Gendo::IdentifiesNPlusOneQueries.identify(
+      [query],
+      minimum_query_occurances: 1
+    )
+
+    expect(identified).to eq([
+      PotentialNPlusOneQuery.new("bars", [query])
+    ])
+  end
+end
