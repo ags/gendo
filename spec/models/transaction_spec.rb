b71f88f5700aa24688745029ac7205fafd9fc87c
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-07-29 21:08:25 +1000

    Spec for Transaction source requirement.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,4 +1,10 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Transaction do
+  it "requires a source" do
+    transaction = Transaction.new
+
+    expect(transaction.valid?).to be_false
+    expect(transaction.errors.messages[:source]).to eq(["can't be blank"])
+  end
 end
