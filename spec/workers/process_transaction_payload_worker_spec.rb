243db09ea30b0c0e408607b81e70893f8b8ff974
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-08-01 00:03:25 +1000

    ProcessTransactionPayloadWorker#process_for_app test.

--- spec/workers/process_transaction_payload_worker_spec.rb
+++ spec/workers/process_transaction_payload_worker_spec.rb
@@ -2,20 +2,35 @@ require "spec_helper"
 require "sidekiq/testing"
 
 describe ProcessTransactionPayloadWorker do
-  # sidekiq serializes data as JSON, so encode and decode our sample hash
-  let(:payload) { JSON.load(transaction_payload_hash.to_json) }
-  let(:app) { App.make! }
-  let(:perform!) {
-    -> { ProcessTransactionPayloadWorker.new.perform(app.id, payload) }
-  }
+  describe "#process_for_app" do
+    it "queues the worker with the given app id and payload" do
+      payload = double(:payload)
+      app = double(:app, id: 123)
 
-  it "creates a Transaction" do
-    expect(perform!).to change { app.transactions.count }.by(+1)
+      expect(ProcessTransactionPayloadWorker).to \
+        receive(:perform_async).
+        with(app.id, payload)
+
+      ProcessTransactionPayloadWorker.process_for_app(app, payload)
+    end
   end
 
-  it "queues an IdentifyNPlusOneQueriesJob" do
-    expect(perform!).to change {
-      IdentifyNPlusOneQueriesWorker.jobs.size
-    }.by(1)
+  describe "#perform" do
+    # sidekiq serializes data as JSON, so encode and decode our sample hash
+    let(:payload) { JSON.load(transaction_payload_hash.to_json) }
+    let(:app) { App.make! }
+    let(:perform!) {
+      -> { ProcessTransactionPayloadWorker.new.perform(app.id, payload) }
+    }
+
+    it "creates a Transaction" do
+      expect(perform!).to change { app.transactions.count }.by(+1)
+    end
+
+    it "queues an IdentifyNPlusOneQueriesJob" do
+      expect(perform!).to change {
+        IdentifyNPlusOneQueriesWorker.jobs.size
+      }.by(1)
+    end
   end
 end
