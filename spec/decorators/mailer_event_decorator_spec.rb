73b5d39f5ceea42909c2d31f8702ccb7961df0a3
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-08-07 21:36:06 +1000

    MailerEventDecorator spec uses require_relative.

--- spec/decorators/mailer_event_decorator_spec.rb
+++ spec/decorators/mailer_event_decorator_spec.rb
@@ -1,11 +1,11 @@
-require 'draper'
-require './spec/support/shared_examples_for_decorates_duration'
-require './spec/support/shared_examples_for_decorates_event_timestamps'
-require './spec/support/shared_examples_for_has_undecorated_class_name'
-require './app/decorators/decorates_event_timestamps'
-require './app/decorators/has_undecorated_class_name'
-require './app/decorators/decorates_duration'
-require './app/decorators/mailer_event_decorator'
+require "draper"
+require_relative "../support/shared_examples_for_decorates_duration"
+require_relative "../support/shared_examples_for_decorates_event_timestamps"
+require_relative "../support/shared_examples_for_has_undecorated_class_name"
+require_relative "../../app/decorators/decorates_event_timestamps"
+require_relative "../../app/decorators/has_undecorated_class_name"
+require_relative "../../app/decorators/decorates_duration"
+require_relative "../../app/decorators/mailer_event_decorator"
 
 Draper::ViewContext.test_strategy :fast
 
