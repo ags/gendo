6cb0f9b03ed7164f7a52ec94ffcf5c47499799f5
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-08-18 23:20:53 +1000

    More complete SqlEvent#cached? spec.

--- spec/models/sql_event_spec.rb
+++ spec/models/sql_event_spec.rb
@@ -2,11 +2,18 @@ require "spec_helper"
 
 describe SqlEvent do
   describe "#cached?" do
-    context "with a name of 'CACHED'" do
+    context "when the event name is 'CACHED'" do
       it "is true" do
         event = SqlEvent.new(name: "CACHED")
 
         expect(event.cached?).to be_true
+      end
+    end
+    context "when the event name is not 'CACHED'" do
+      it "is false" do
+        event = SqlEvent.new(name: "SqlEvent Load")
+
+        expect(event.cached?).to be_false
       end
     end
   end
