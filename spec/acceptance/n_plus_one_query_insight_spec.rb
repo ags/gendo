f42ce2de12bfa9eab7bf7db8224de074ed191500
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-09-20 09:20:17 +1000

    Re-write "n+1 query insight" spec with capybara rspec.

---
+++ spec/acceptance/n_plus_one_query_insight_spec.rb
@@ -0,0 +1,31 @@
+require "acceptance_spec_helper"
+
+feature "n+1 query insight" do
+  let(:user) { User.make! }
+  let(:app) { App.make!(user: user) }
+  let(:source) { Source.make!(app: app) }
+  let(:request) { Request.make!(:with_zero_runtime, source: source) }
+  let!(:n_plus_one_query) { NPlusOneQuery.make!(request: request) }
+
+  background do
+    sign_in_as(user)
+  end
+
+  scenario "Viewing a Source with a recent n+1 query" do
+    visit app_source_path(source.app, source)
+
+    expect_to_see "pattern of an n+1 query"
+  end
+
+  scenario "Viewing a Request with an n+1 query" do
+    visit app_request_path(request.app, request)
+
+    expect_to_see "Detected an n+1 query"
+  end
+
+  scenario "Viewing the n+1 query insight" do
+    visit app_n_plus_one_query_path(n_plus_one_query.app, n_plus_one_query)
+
+    expect_to_see "This may be an n+1 query"
+  end
+end
