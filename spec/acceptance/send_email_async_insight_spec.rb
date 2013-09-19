7e3663a9557bf395f1c032e904cda80b3cb17d62
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-09-20 09:25:36 +1000

    Re-write "send email asynchronously insight" spec with capybara rspec.

---
+++ spec/acceptance/send_email_async_insight_spec.rb
@@ -0,0 +1,25 @@
+require "acceptance_spec_helper"
+
+feature "Send email asynchronously insight" do
+  let(:user) { User.make! }
+  let(:app) { App.make!(user: user) }
+  let(:source) { Source.make!(app: app) }
+  let(:request) { Request.make!(:with_zero_runtime, source: source) }
+  let!(:mailer_event) { MailerEvent.make!(request: request) }
+
+  background do
+    sign_in_as(user)
+  end
+
+  scenario "Viewing a source that recently sent an email in a request" do
+    visit app_source_path(source.app, source)
+
+    expect_to_see "Looks like you're sending emails in your requests"
+  end
+
+  scenario "Viewing the request that sent an email" do
+    visit app_request_path(request.app, request)
+
+    expect_to_see "Send FooMailer asynchronously"
+  end
+end
