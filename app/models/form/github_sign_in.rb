2d21b1b694dba61ea615369afd742f2e8b3d4c23
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-10-07 08:35:21 +1100

    Prefer not to access attributes as instance variables.

--- app/models/form/github_sign_in.rb
+++ app/models/form/github_sign_in.rb
@@ -14,17 +14,21 @@ module Form
     def save!
       @user = GithubUser.find_or_initialize(github_access_token)
 
-      send_welcome_email = @user.new_record?
+      send_welcome_email = user.new_record?
 
-      @user.save!
+      user.save!
 
       if send_welcome_email
-        MailUserWelcomeWorker.welcome(@user)
+        MailUserWelcomeWorker.welcome(user)
       end
 
-      @authenticator.sign_in(@user)
+      authenticator.sign_in(user)
 
       true
     end
+
+    private
+
+    attr_reader :authenticator
   end
 end
